e32881ce3b3f18fd2b66e7e7c7850a457f503a32
BEYZA AKTAŞ <134061187+Beyzakttas@users.noreply.github.com>
2025-07-10 14:52:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+// http://api.weatherapi.com/v1/current.json?key=bccd33fd304a492aa4472716251007&q=Turkey&aqi=no
+const tempretureField=document.querySelector(".temp");
+const locationField=document.querySelector(".time_location p");
+const dateandTimeField=document.querySelector(".time_location span");
+const conditionField=document.querySelector(".condition p");
+const searchField=document.querySelector(".search_area");
+const form =document.querySelector('form')
+form.addEventListener('submit',searchForLocation)
+let target="turkey"
+const fetchResults=async (targetLocation)=>{
+let url=`http://api.weatherapi.com/v1/current.json?key=bccd33fd304a492aa4472716251007&q=${targetLocation}&aqi=no`
+
+const res=await fetch(url)
+const data= await res.json()
+console.log(data)
+let locationName=data.location.name
+let time=data.location.localtime
+let temp=data.current.temp_c
+let condition=data.current.condition.text
+updateDetails(temp ,locationName,time,condition)
+}
+function updateDetails(temp,locationName,time,condition) { 
+   let splitDate=time.split(" ")[0];
+   let splitTime=time.split(" ")[1];
+   let currentDay=getDayName(new Date(splitDate).getDay())
+   
+    tempretureField.innerText=temp
+    locationField.innerText=locationName
+   dateandTimeField.innerText=`${splitDate} ${currentDay} ${splitTime}`;
+   conditionField.innerText=condition
+
+ }
+function searchForLocation(e) {  
+    e.preventDefault()
+    target=searchField.value
+    fetchResults(target)
+}
+fetchResults(target)
+function getDayName(number){
+    switch(number){
+        case 0:
+            return 'sunday';
+              case 1:
+            return 'monday';
+              case 2:
+            return 'tursday';
+              case 3:
+            return 'wednesday';
+              case 4:
+            return 'thursday';
+              case 5:
+            return 'friday';
+              case 6:
+            return 'saturday';
+           
+            
+            
+    }
+}
